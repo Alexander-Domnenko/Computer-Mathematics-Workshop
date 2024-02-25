e577114d5f7933fca3c46a9205de687e1af2c7bb
Alexander-Domnenko <91257943+Alexander-Domnenko@users.noreply.github.com>
2024-02-25 12:10:06 +0800

    Add files via upload

---
+++ the-Gauss-Jordan-method-for-equations/sle_test.py
@@ -0,0 +1,21 @@
+import pytest
+from sle import *
+
+@pytest.mark.parametrize("a,b,expected_result", [([[2.0, 3.0], [4.0, 3.0]], [[2.0], [7.0]], [[2.5], [-1.0]])])
+def test_sle_2d_1(a, b, expected_result):
+    assert sle(a, b) == expected_result
+
+@pytest.mark.parametrize("a,b,expected_result", [([[1, -6], [5, 6]], [[17], [13]], [[5.0], [-2.0]])])
+def test_sle_2d_2(a, b, expected_result):
+    assert sle(a, b) == expected_result
+
+
+@pytest.mark.parametrize("a,b,expected_result", [([[-1, 2, 6], [3, -6, 0], [1, 0, 6]],[[15], [-9], [5]],[[-7.0], [-2.0], [2.0]])])
+def test_sle_3d_1(a, b, expected_result):
+    assert sle(a, b) == expected_result
+
+@pytest.mark.parametrize("a,b,expected_result", [([[1, 1, 1], [1, 2, 3], [1, 3, 6]],[[3], [6], [10]],[[1.0], [1.0], [1.0]])])
+def test_sle_3_2(a, b, expected_result):
+    assert sle(a, b) == expected_result
+
+
